058dabcd62ef9ec6051bd2627261c2d447824960
Jonalyn Llanes <jllanes2@my.centennialcollege.ca>
2024-01-24 10:24:59 -0500

    Update: Home Page

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,4 +1,29 @@
+import { Link } from 'react-router-dom';
+import '../src/index.css'
+
 export default function Home() {
-    return 
+    return <>
+        <aside>
+            <div>
+                <h1>Hello there, </h1> 
+                <h2>Jonalyn here</h2>
+                <h2> I am an aspiring web developer </h2>
+            </div>
+        </aside>
+
+        <section>
+            <div>
+                <p>Welcome! </p>
+                <h1>Elevating web experiences with creativity</h1>
+            </div>
+
+            <div>
+                <h4>To know more about me</h4>
+                <Link to="/about">
+                    <button>Click here</button>
+                </Link>
+            </div>
+        </section>
+    </>
 }
     
--- client/components/layout.jsx
+++ client/components/layout.jsx
@@ -1,16 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../src/assets/Logo.png';
 
 export default function Layout() {
     return (
         <>
-            <h1>My Portfolio</h1>
-                <nav>
-                    <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/education">Education</Link>| <Link to="/project">Project</Link>| <Link to="/contact">Contact</Link>
-                </nav>
-            <br/>
+            <nav>
+                <div className='container'>
+                    <Link to ="/"><img src = {Logo} alt = "Company Logo" className = "logo" width = "60px" height= "60px"/>  </Link>
+                
+
+                <div className='nav-elements'>
+                    <ul>
+                        
+
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/education">Education</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/project">Project</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                        
+                    </ul>
+                    </div>
+                </div>    
+            </nav>
+
+
+            <footer>
+                <div>
+                    <p>This is  a footer </p>
+                    <p>&copy; 2024 Jonalyn. All Rights Reserved.</p>
+                </div>
+            </footer>
         </>  
     );
+    
 }
 
 
